ecc06b84ff94ae8b569dd8daaf2ea7b0a6cff1f4
Kate Saslow <39833858+ksaslow@users.noreply.github.com>
2024-02-08 16:28:31 -0500

    container components

--- src/pages/qcomps/props_item.js
+++ src/pages/qcomps/props_item.js
@@ -1,9 +1,20 @@
-function Item({ name, isPacked }) {
+function ItemOld({ name, isPacked }) { //changed name to ItemOld instead of Item to see if tern. op worked
   if (isPacked) {
-    return <li className="item">{name} ✔</li>;
+    return <li className="item"> <del>{name}</del> </li>;
   }
   return <li className="item"> {name} </li>
 }
+
+// try this with a ternary operator!
+function Item({ name, isPacked }) {
+  let tag;
+  isPacked? tag = <li> <del> {name} </del> </li> : tag = <li> {name} </li>;
+  return tag;
+}
+
+// Jpydeep's example:
+
+
 
 export default function PackingList() {
   return (
