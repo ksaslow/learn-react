23d63758d9ae22d9360b1bb21c303f5dd8ca57f9
Kate Saslow <39833858+ksaslow@users.noreply.github.com>
2024-02-15 16:43:46 -0500

    changes from class

--- src/pages/components/queueUpdates.js
+++ src/pages/components/queueUpdates.js
@@ -7,9 +7,10 @@ export default function Counter() {
     <>
       <h1>{number}</h1>
       <button onClick={() => {
-        setNumber(n => n + 1);
-        setNumber(n => n + 1);
-        setNumber(n => n + 1);
+        // this is called BATCHING UPDATES
+        setNumber(n => n + 1); // instead of passing a new number (like in Counter example), we use the arrow function
+        setNumber(n => n + 1); // the arrow function tells react to put the function in a QUEUE!! AND returns a value! 
+        setNumber(n => n + 1); // setNumber(2)
       }}>+3</button>
     </>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,16 +7,30 @@ import MyComp from './qcomps/firstcomp'
 import Bio from './qcomps/bios'
 import Profile from './components/profile_props'
 //import Gallery from './components/gallery' // note that Profile must match exactly how component is exported in profile.js
-import Gallery from './qcomps/gallery_props'
+// import Gallery from './qcomps/gallery_props'
 import Holder from './components/square'
 // import PackingList from './components/props_item'
 import PackingList from './qcomps/props_item'
 import TodoList from './qcomps/todos'
+// import List from './components/list_plain'
+// import List from './components/list_keys'
+import List from './qcomps/list_keys_id'
+import ImagesList from './qcomps/list_keys_id'
+import RecipeList from './qcomps/recipes'
+// import Gallery from './components/whyState'
+import Gallery from './components/state'
+// import Gallery from './qcomps/state'
+import PersonData from './components/stateObj'
+//cimport Form from './qcomps/stuckForm'
+import FeedbackForm from './qcomps/thankYouCrash'
+import Counter from './components/queueUpdates'
+import Form from './components/snapshot2'
+import RequestTracker from './qcomps/shoppingCart'
 
 export default function Home() { // component called "Home": see styles > Home.module.css
   return (
     <div className={styles.main}>
-        <PackingList /> 
+        <RequestTracker /> 
     </div>
   )
 }
