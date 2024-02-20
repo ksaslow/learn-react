9c4936a2df5d652307703aa343389f04f9c44906
Kate Saslow <39833858+ksaslow@users.noreply.github.com>
2024-02-20 12:36:07 -0500

    fixed class activity artistsRemove

--- src/pages/qcomps/artistsRemoveArr.js
+++ src/pages/qcomps/artistsRemoveArr.js
@@ -11,6 +11,12 @@ export default function List() {
     initialArtists
   );
 
+  function handleDeleteClick (id){
+    setArtists(artists.filter((artist) => {
+      return artist.id != id;
+    }));
+  }
+
   return (
     <>
       <h1>Inspiring sculptors:</h1>
@@ -19,7 +25,9 @@ export default function List() {
           <li key={artist.id}>
             {artist.name}{' '}
             <button onClick={() => {
-              artists.splice(artist.id, 1)
+              handleDeleteClick(artist.id);
+              // artists.splice(artist.id, 1) // cant use splice 
+              // because splice is a mutable operation!
             }}>
               Delete
             </button>
